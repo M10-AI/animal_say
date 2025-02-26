b67087dfa30a4f196b9671cc5866d35613d0b04a
M10-AI <m10ai@proton.me>
2025-02-26 11:24:43 +0100

    best practices

--- exercise.py
+++ exercise.py
@@ -1,23 +1,44 @@
 import streamlit as st
-
 class Animal:
 
-    def __init__(self, legs):
-        self.legs = legs
+    def __init__(self, legs): # Constructor
+        self.__legs = legs # Encapsulation: legs attribute cannot be accessed publicly
+
+    @property
+    def legs(self):
+        return self.__legs
+
+    @legs.setter
+    def legs(self, legs):
+
+        if isinstance(legs, int) and legs >= 0:
+            self.__legs = legs
+        else:
+            raise ValueError("Fuck you")        
 
     def speak(self):
         raise NotImplementedError("Subclass must implement abstract method")
 
-    def move():
+    def move(self):
         raise NotImplementedError("Subclass must implement abstract method")
 
 
-class Dog(Animal):
+class Dog(Animal): # Inheritance
 
-    def speak(): #Method overriding
+    def speak(self): # Abstraction & Method overriding 
         st.write("Woff")
     
-    def move(self): #Method overriding
-        print(f"Move with {self.legs} legs")
+    def move(self): # Abstraction &  Method overriding
+        st.write(f"Move with {self.legs} legs") # Polymoprhism
+
+# second animal
+
+# third animal
+
+# fourth animal
+
+# fifth animal
+
+
 
 
